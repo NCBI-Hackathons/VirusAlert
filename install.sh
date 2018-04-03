453e0f371c2a0b5b6604ee6c12a20de323c95f08
Ubuntu <tagomberg@berkeley.edu>
2018-04-03 17:27:35 +0000

    add untested virfinder install script

--- install.sh
+++ install.sh
@@ -3,22 +3,22 @@
 SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 DATA_DIR="$SCRIPT_DIR/../data"
 TOOLS_DIR="$SCRIPT_DIR/../tools"
-export PATH=$TOOLS_DIR:$PATH
-pushd "$TOOLS_DIR"
-wget "http://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-centos_linux64.tar.gz"
-tar -xzf sratoolkit.current-centos_linux64.tar.gz
-popd 
-pushd "$DATA_DIR"
-echo "input the SRRs of the data you would like to work with (now will just be our data)"
-touch HBV01wVirus.fasta
-touch HBV02control.fasta
-touch MecqueEbola.fasta
-/home/ubuntu/LongReadViruses/tools/sratoolkit.2.9.0-centos_linux64/bin/fastq-dump -X 5 -Z SRR5383888 > HBV01wVirus
-/home/ubuntu/LongReadViruses/tools/sratoolkit.2.9.0-centos_linux64/bin/fastq-dump -X 5 -Z SRR5383891 > HBV02control
-/home/ubuntu/LongReadViruses/tools/sratoolkit.2.9.0-centos_linux64/bin/fastq-dump -X 5 -Z SRR5150787 > MecqueEbola
+# export PATH=$TOOLS_DIR:$PATH
+# pushd "$TOOLS_DIR"
+# wget "http://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-centos_linux64.tar.gz"
+# tar -xzf sratoolkit.current-centos_linux64.tar.gz
+# popd 
+# pushd "$DATA_DIR"
+# echo "input the SRRs of the data you would like to work with (now will just be our data)"
+# touch HBV01wVirus.fasta
+# touch HBV02control.fasta
+# touch MecqueEbola.fasta
+# /home/ubuntu/LongReadViruses/tools/sratoolkit.2.9.0-centos_linux64/bin/fastq-dump -X 5 -Z SRR5383888 > HBV01wVirus
+# /home/ubuntu/LongReadViruses/tools/sratoolkit.2.9.0-centos_linux64/bin/fastq-dump -X 5 -Z SRR5383891 > HBV02control
+# /home/ubuntu/LongReadViruses/tools/sratoolkit.2.9.0-centos_linux64/bin/fastq-dump -X 5 -Z SRR5150787 > MecqueEbola
  
+# TODO install python-biopython?
  
 popd
  
 echo "installing virusfriends..."
-
--- virfinder/install.sh
+++ virfinder/install.sh
@@ -5,4 +5,16 @@ SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 DATA_DIR="$SCRIPT_DIR/../data"
 TOOLS_DIR="$SCRIPT_DIR/../tools"
 
+# fetch R package source
+echo "downloading virfinder..."
+pushd "$TOOLS_DIR"
+wget https://codeload.github.com/jessieren/VirFinder/zip/master 
+unzip Virfinder-master.zip
+mv Virfiner-master/linux/VirFinder ./
+rm -r Virfinder-master*
+popd
+
+# install R package + dependencies
 echo "installing virfinder..."
+sudo apt-get -y install r-base
+Rscript "install.R"
