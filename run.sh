540af3edbbfe8fc30bd280b29cfe4983a5b740b3
Ubuntu <tagomberg@berkeley.edu>
2018-04-03 21:29:47 +0000

    start rearranging packaging

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for step in virfinder blast rpsblast xgboost; do
-  ./${step}/run.sh 2>&1 | tee ${step}/run.log
-done
--- xgboost/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# comment out when done working on it:
-SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
-DATA_DIR="$SCRIPT_DIR/../data"
-TOOLS_DIR="$SCRIPT_DIR/../tools"
-
-echo "installing xgboost..."
