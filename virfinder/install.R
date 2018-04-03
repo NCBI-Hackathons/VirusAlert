6fff3779cd7333e01881a804e4a4ca266d03d466
Ubuntu <tagomberg@berkeley.edu>
2018-04-03 22:12:43 +0000

    virfinder install

--- virfinder/install.R
+++ virfinder/install.R
@@ -1,7 +1,19 @@
 #!/usr/bin/Rscript
 
-install.packages("glmnet", dependencies=TRUE)
-install.packages("Rcpp", dependencies=TRUE)
+# see https://www.r-bloggers.com/permanently-setting-the-cran-repository/
+local({
+  r <- getOption("repos")
+  r["CRAN"] <- "http://cran.cnr.berkeley.edu/"
+  options(repos = r)
+})
+
+toolsdir=Sys.getenv('TOOLS_DIR')
+libdir=file.path(toolsdir, 'R')
+srcdir=file.path(toolsdir, 'VirFinder')
+
+install.packages("glmnet", dependencies=TRUE, lib=libdir)
+install.packages("Rcpp", dependencies=TRUE, lib=libdir)
+
 source("https://bioconductor.org/biocLite.R")
 biocLite("qvalue")
-install.packages(Sys.getenv('TOOLS_DIR'), repos=NULL, type='source')
+install.packages(srcdir, repos=NULL, type='source', lib=libdir)
