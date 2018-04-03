61d7d037128393df41e38e0960c80d7711ed32a5
Jeffrey David Johnson <jefdaj@berkeley.edu>
2018-04-03 00:20:24 +0000

    add top-level data and tools dirs

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-for step in virfinder blast rpsblast xgboost; do
+export ROOT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+export DATA_DIR="$ROOT_DIR/data"
+export TOOLS_DIR="$ROOT_DIR/tools"
+
+for step in virusfriends virfinder blast rpsblast xgboost; do
   ./${step}/install.sh 2>&1 | tee ${step}/install.log
 done
