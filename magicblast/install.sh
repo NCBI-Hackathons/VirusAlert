b24d80bfa6cbbb163dc64004adff39d2b210984b
Ubuntu <tagomberg@berkeley.edu>
2018-04-03 21:45:07 +0000

    package magicblast

--- magicblast/install.sh
+++ magicblast/install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-# comment out when done working on it:
 SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 DATA_DIR="$SCRIPT_DIR/../data"
 TOOLS_DIR="$SCRIPT_DIR/../tools"
 
-echo "installing rpsblast..."
+# based on https://github.com/NCBI-Hackathons/VirusFriends
+echo "installing magicblast..."
+mkdir -p "$TOOLS_DIR"
+cd "$TOOLS_DIR"
+wget ftp://ftp.ncbi.nlm.nih.gov/blast/executables/magicblast/LATEST/ncbi-magicblast-1.3.0-x64-linux.tar.gz -O magicblast.tar.gz
+tar -xvf magicblast.tar.gz
+rm -f magicblast.tar.gz
+P=$(find . -name magicblast | sed -e 's/magicblast$//; s/^\.\///')
+echo "magicblast installed in $NEWPATH:$PWD/$P"
