c239fb59782b1dfaaf1c68f501ca67aa91cc494c
Ubuntu <tagomberg@berkeley.edu>
2018-04-03 18:36:56 +0000

    resolved merge issues

--- virusfriends/install.sh
+++ virusfriends/install.sh
@@ -1,5 +1,5 @@
-!/bin/bash
-
+/bin/bashi
+ 
 # comment out when done working on it:
 SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 DATA_DIR="$SCRIPT_DIR/../data"
@@ -11,7 +11,7 @@ tar -xzf sratoolkit.current-centos_linux64.tar.gz
 popd 
 pushd "$DATA_DIR"
 echo "input the SRRs of the data you would like to work with (now will just be our data)"
-<<<<<<< HEAD
+ 
 touch HBV01wVirus.sam
 touch HBV02control.sam
 touch MecqueEbola.sam
@@ -19,22 +19,19 @@ mkdir sra_sams
 pushd sra_sams
 for i in /home/ubuntu/LongReadViruses/virusfriends/test/sra_ids.txt
 do
-	touch $i.sam
-	/opt/VirusFriends/tools/sratoolkit.2.9.0-centos_linux64/bin/sam-dump $i > $i.sam
+    
+        /opt/VirusFriends/tools/sratoolkit.2.9.0-centos_linux64/bin/sam-dump view -S -b $i.sam > $i.sam
+        mv $i.sam /opt/VirusFriends/analysis/$i/$i.sam
+    
+ 
 done
 /home/ubuntu/LongReadViruses/tools/sratoolkit.2.9.0-centos_linux64/bin/sam-dump SRR5383888 > HBV01wVirus.sam
 /home/ubuntu/LongReadViruses/tools/sratoolkit.2.9.0-centos_linux64/bin/sam-dump SRR5383891 > HBV02control.sam
 /home/ubuntu/LongReadViruses/tools/sratoolkit.2.9.0-centos_linux64/bin/sam-dump SRR5150787 > MecqueEbola.sam
-=======
-touch HBV01wVirus.fasta
-touch HBV02control.fasta
-touch MecqueEbola.fasta
-/home/ubuntu/LongReadViruses/tools/sratoolkit.2.9.0-centos_linux64/bin/fastq-dump -X 5 -Z SRR5383888 > HBV01wVirus
-/home/ubuntu/LongReadViruses/tools/sratoolkit.2.9.0-centos_linux64/bin/fastq-dump -X 5 -Z SRR5383891 > HBV02control
-/home/ubuntu/LongReadViruses/tools/sratoolkit.2.9.0-centos_linux64/bin/fastq-dump -X 5 -Z SRR5150787 > MecqueEbola
->>>>>>> a26f2b4dadff81476a3e89b31d4b9c63c5d9970c
-
-
+ 
+ 
+ 
 popd
-
+ 
 echo "installing virusfriends..."
+
