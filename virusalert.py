68a79ec639dfbc6a7e6e4981436d79d22b6f57b6
DailyDreaming <lblauvel@ucsc.edu>
2018-04-04 13:39:39 -0700

    Update virusalert.py

--- virusalert.py
+++ virusalert.py
@@ -16,28 +16,9 @@ def main():
     #     args = docopt(f.read(), version='longreadviruses 0.1')
     # print(parse(args))
 
-    # only fetch the first 10 entries in the fastq
-    max_entries = 10
-
     v = VirLib()
-    # fetch a fastq local path
-    fastq_path = v.processInput(type='srr', input='SRR6172653')
-    # return a list of BioPython objects representing each fastq entry in the file
-    entries = v.entriesFromFastq(fastq=fastq_path)
-    # BLAST, generate an XML, and parse to obtain the first species hit
-    list_of_species = []
-    for record in entries[:max_entries]:
-        output_xml_path = v.blastAPISearch(record, output_xml_path='default.xml')
-        species = v.fetchXMLResults(output_xml_path)
-        list_of_species.append(species)
-
-    # generate the output results in a file
-    with open('species_results.txt', 'w') as f:
-        print('Top hits by p-value in sample are: ')
-        for s in list_of_species:
-            f.write(s)
-            f.write('\n')
-            print(s)
+    # requires srr code or fastq
+    v.run(type='srr', input='SRR6172653')
 
 if __name__ == '__main__':
     main()
