4f54e292158b73fb34ddaf4686cd7d71abdc862a
Jeffrey David Johnson <jefdaj@berkeley.edu>
2018-04-04 13:53:45 -0700

    virusalert.py

--- virusalert.py
+++ virusalert.py
@@ -6,19 +6,19 @@ from virLib import VirLib
 def parse(args):
     return {
         'verbose' : args['-v'],
-        'inputs'  : args['-i'],
+        'input'   : args['-i'],
         'outdir'  : args['-o'],
         'intype'  : args['-t']
     }
 
 def main():
-    # with open('usage.txt', 'r') as f:
-    #     args = docopt(f.read(), version='longreadviruses 0.1')
+    with open('usage.txt', 'r') as f:
+        args = docopt(f.read(), version='longreadviruses 0.1')
     # print(parse(args))
 
     v = VirLib()
-    # requires srr code or fastq
-    v.run(type='srr', input='SRR6172653')
+    for i in args['input']:
+        v.run(type=args['intype'], input=i)
 
 if __name__ == '__main__':
     main()
