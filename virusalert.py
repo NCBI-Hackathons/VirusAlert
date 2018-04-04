1fd8dcc399d281fcfdc73cbfedaac52aa3cb98d1
Jeffrey David Johnson <jefdaj@berkeley.edu>
2018-04-04 15:19:48 -0700

    virusalert

--- virusalert.py
+++ virusalert.py
@@ -3,50 +3,71 @@
 from docopt import docopt
 from virLib import VirLib
 from Bio import SeqIO
-import os
+from subprocess import check_call
+# import os
+from os.path import dirname, join, realpath
+
+def datapath(path):
+    return join(dirname(realpath(__file__)), 'data', path)
+
+def toolpath(path):
+    return join(dirname(realpath(__file__)), 'tools', path)
 
 def parse(args):
     contdb = args['-d']
-    if contdb is None:
-        contdb = os.path.realpath('viralg')
+    if not contdb:
+        contdb = datapath('viralg')
     return {
         'verbose' : args['-v'],
         'input'   : args['-i'],
         'cutoff'  : args['-c'],
         'contdb'  : contdb,
-        'outdir'  : args['-o'],
+        'outdir'  : datapath(''),
         'intype'  : args['-t']
     }
 
-def magicblast(args):
-    cmd = ["magicblast", "-query",
-            args['input'], 
-            "-db", args['contdb'],
-            "-out", os.path.join(args['outdir'], "user_magicB.gz"),
-            "-gzo -outfmt tabular"]
+def getinput(v, args):
+    print(args)
+    t = args['intype']
+    if t == 'srr':
+      i = v.processInput(type=t, input=args['input'])
+      return i
+    elif t == 'fastq':
+      i = v.processInput(type=t, input=args['input'])
+      SeqIO.convert(args['input'], "fastq", i, "fasta")
+      return i
+    elif t == 'fasta':
+        return(args['input'])
+    else:
+        raise Exception('invalid intype: ' + t)
 
+def magicblast(v, args):
+    i = getinput(v, args)
+    mb = toolpath('ncbi-magicblast-1.3.0/bin/magicblast')
+    cmd = [mb, "-query",
+            args['input'],
+            "-db", args['contdb'],
+            "-out", join(args['outdir'], "magicblast_hits.gz"),
+            "-gzo -outfmt tabular"]
+    print(cmd)
     check_call(cmd)
 
 def virfinder(args):
     cmd = ["runVirFinder.R", args['input'], args['pvalue']]
     check_call(cmd)
 
-def virlib(args):
-    v = VirLib(verbose=True)
-    for i in args['input']:
-        v.run(type=args['intype'], input=i)
+# def virlib(args):
 
 def main():
-    # fetch SRR files
-    # i = v.processInput(type='srr', input='SRR5383888')
-    # print(i)
-    
-    # SeqIO.convert("example.fastq", "fastq", "example.fasta", "fasta")
     with open('usage.txt', 'r') as f:
-        args = docopt(f.read(), version='virusalert 1.0')
-    os.chdir(args['outdir'])
-    virfinder(args)
-    virlib(args)
+        args = parse(docopt(f.read(), version='virusalert 1.0'))
+    v = VirLib(verbose=True)
+    # print(args)
+    # virfinder(args)
+    magicblast(v, args)
+    # virlib(args)
+    # for i in args['input']:
+    v.run(type=args['intype'], input=args['input'])
 
 if __name__ == '__main__':
     main()
